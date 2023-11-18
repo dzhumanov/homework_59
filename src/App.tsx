54398aecddb119b775fa00f18e1998151dcfaa37
Temirlan Dzhumanov <temadjumanov@gmail.com>
2023-11-18 14:07:12 +0600

    Created second task logic, added JokeButton component that doesn't render when clicked. finished second task with one additional task

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import Form from "./Components/Form/Form";
 import WatchList from "./Components/WatchList/WatchList";
 import { MovieProps } from "./types";
+import JokeButton from "./Components/JokeButton/JokeButton";
+
+const url = "https://api.chucknorris.io/jokes/random";
 
 function App() {
   const [movies, setMovies] = useState<MovieProps[]>([]);
@@ -26,12 +29,43 @@ function App() {
     setMovies((prevMovies) => prevMovies.filter((movie) => movie.id !== id));
   }, []);
 
+  // task 2
+  const useJoke = () => {
+    const [joke, setJoke] = useState<string>("");
+
+    const fetchJoke = useCallback(async () => {
+      const response = await fetch(url);
+
+      if (response.ok) {
+        const data = await response.json();
+        setJoke(data.value);
+      }
+    }, []);
+
+    useEffect(() => {
+      void fetchJoke();
+    }, [fetchJoke]);
+
+    return { joke, fetchJoke };
+  };
+
+  const { joke, fetchJoke } = useJoke();
+
   return (
     <>
       <div className="container w-50">
         <div className="form-container">
-          <Form onAdd={addMovie}/>
-          <WatchList movies={movies} onEdit={editMovie} onDelete={deleteMovie}/>
+          <Form onAdd={addMovie} />
+          <WatchList
+            movies={movies}
+            onEdit={editMovie}
+            onDelete={deleteMovie}
+          />
+        </div>
+        <hr />
+        <div className="joke-container">
+          <div>{joke}</div>
+          <JokeButton fetchJoke={fetchJoke}/>
         </div>
       </div>
     </>
---
+++ src/Components/JokeButton/JokeButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {
+  fetchJoke: () => void;
+}
+
+const JokeButton: React.FC<Props> = React.memo(({ fetchJoke }) => {
+    console.log('button rendered')
+  return (
+    <button className="btn btn-primary" onClick={fetchJoke}>
+      Generate new joke
+    </button>
+  );
+});
+
+export default JokeButton;
